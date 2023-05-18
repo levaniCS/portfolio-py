577a80c7388c90fe079393edfed551c7ba8eca8c
levaniCS <levanisarishvili3105@gmail.com>
2023-05-18 15:24:28 +0400

    pUSH PRROJECT

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data, filename = 'database.csv'):
+    with open(filename, mode='a', newline='') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', lineterminator='\n', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, subject, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            print(data)
+            write_to_file(data)
+            return redirect('/thankyou.html')
+        except:
+            return 'did not saved to db'
+    else:
+        return 'something went wrong. Please try again'
